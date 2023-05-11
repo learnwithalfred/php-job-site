3dcafa626dfc0de44c2e7d83914a54ebafad02d4
Alfred Boateng <kwadwo.boateng.alfred@gmail.com>
2023-05-11 04:18:24 +0000

    updated

--- index.php
+++ index.php
@@ -42,7 +42,17 @@ $job_count = mysqli_num_rows($result);
 
 
 
+  <div
+    class="bg-white dark:bg-gray-900 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
 
+      <h1
+        class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Find jobs near me</h1>
+      <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">Here at Flowbite
+        <?php echo $job_count; ?> full-time and remote jobs hiring now</p>
+    </div>
+  </div>
 
 
 
